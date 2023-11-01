5cae4ef74e9fb64f8153cc9a20f1fab67dff0830
gajalakshmi2901 <gajalakshmi2901@gmail.com>
2023-11-01 13:23:00 +0530

    updating

--- context.tf
+++ context.tf
@@ -10,16 +10,12 @@ module "this" {
   stage               = var.stage
   name                = var.name
   delimiter           = var.delimiter
-  attributes          = var.attributes
   tags                = var.tags
-  additional_tag_map  = var.additional_tag_map
   label_order         = var.label_order
   regex_replace_chars = var.regex_replace_chars
   id_length_limit     = var.id_length_limit
   label_key_case      = var.label_key_case
   label_value_case    = var.label_value_case
-  descriptor_formats  = var.descriptor_formats
-  labels_as_tags      = var.labels_as_tags
 
   context = var.context
 }
